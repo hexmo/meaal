58f86200685208ea086839560ced60add0d6fe17
hexmo <35497915+hexmo@users.noreply.github.com>
2022-03-13 18:56:25 +0545

    feat(onboarding): :lipstick: UI for gender, height and weight screens

--- src/components/biodata/Weight.tsx
+++ src/components/biodata/Weight.tsx
@@ -1,10 +1,51 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View } from "react-native";
+import React, { useState } from "react";
+import { Text, TextInput } from "react-native-rapi-ui";
+import NextButton from "../NextButton";
 
-const Weight = () => {
+const Weight = ({ navigation }) => {
+  const [weight, setWeight] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleNext = () => {
+    setLoading(true);
+    setTimeout(() => navigation.navigate("FoodPreferances"), 1000);
+  };
+
   return (
-    <View>
-      <Text>Weight</Text>
+    <View style={{ flex: 1, justifyContent: "center", paddingHorizontal: 20 }}>
+      <Text
+        fontWeight="bold"
+        size="h3"
+        style={{
+          alignSelf: "center",
+        }}
+      >
+        Weight
+      </Text>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          paddingHorizontal: "30%",
+        }}
+      >
+        <TextInput
+          containerStyle={{ marginVertical: 20 }}
+          placeholder="00"
+          value={weight}
+          autoCapitalize="none"
+          autoCompleteType="off"
+          autoCorrect={false}
+          keyboardType="number-pad"
+          onChangeText={(text: string) => setWeight(text)}
+          style={{ flex: 1 }}
+        />
+        <Text fontWeight="bold" size="lg" style={{ marginLeft: 10 }}>
+          KG
+        </Text>
+      </View>
+      <NextButton loading={loading} onPress={handleNext} />
     </View>
   );
 };
