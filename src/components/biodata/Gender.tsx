6d8c20535858fe3c9dd03d2f2ecacd650114605c
hexmo <35497915+hexmo@users.noreply.github.com>
2022-03-13 18:56:25 +0545

    feat(onboarding): :lipstick: UI for gender, height and weight screens

--- src/components/biodata/Gender.tsx
+++ src/components/biodata/Gender.tsx
@@ -1,15 +1,20 @@
-import { View } from "react-native";
+import { View, Pressable, StyleSheet } from "react-native";
 import React, { useState } from "react";
-import { TextInput, Text } from "react-native-rapi-ui";
+import { themeColor, Text } from "react-native-rapi-ui";
 import NextButton from "../NextButton";
+import { Foundation } from "@expo/vector-icons";
 
-const Gender = () => {
+const Gender = ({ navigation }) => {
   const [gender, setGender] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
 
-  const handleGenderSelect = (gender:string) => {
+  const handleGenderSelect = (gender: string) => {
     setGender(gender);
-    setTimeout(() => setLoading(false), 2000);
+  };
+
+  const handleNext = () => {
+    setLoading(true);
+    setTimeout(() => navigation.navigate("Height"), 1000);
   };
 
   return (
@@ -19,15 +24,50 @@ const Gender = () => {
         size="h3"
         style={{
           alignSelf: "center",
+          marginBottom: 40,
         }}
       >
         Select your gender
       </Text>
-      
-      
 
+      <Pressable
+        style={[styles.genderIcon, gender == "male" ? styles.selected : null]}
+        onPress={() => setGender("male")}
+      >
+        <Foundation name="male-symbol" size={100} color="#21A8FA" />
+      </Pressable>
+      <Pressable
+        style={[styles.genderIcon, gender == "female" ? styles.selected : null]}
+        onPress={() => setGender("female")}
+      >
+        <Foundation name="female-symbol" size={100} color="#F62681" />
+      </Pressable>
+      <NextButton loading={loading} onPress={handleNext} />
     </View>
   );
 };
 
 export default Gender;
+
+const styles = StyleSheet.create({
+  genderIcon: {
+    width: 100,
+    marginBottom: 20,
+    borderRadius: 25,
+    alignSelf: "center",
+    alignItems: "center",
+    backgroundColor: themeColor.white200,
+  },
+  selected: {
+    backgroundColor: themeColor.white,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 10,
+      height: 10,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 10,
+
+    elevation: 16,
+  },
+});
--- src/components/biodata/Height.tsx
+++ src/components/biodata/Height.tsx
@@ -1,10 +1,51 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View } from "react-native";
+import React, { useState } from "react";
+import { Text, TextInput } from "react-native-rapi-ui";
+import NextButton from "../NextButton";
 
-const Height = () => {
+const Height = ({ navigation }) => {
+  const [height, setHeight] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleNext = () => {
+    // setLoading(true);
+    setTimeout(() => navigation.navigate("Weight"), 1000);
+  };
+
   return (
-    <View>
-      <Text>Height</Text>
+    <View style={{ flex: 1, justifyContent: "center", paddingHorizontal: 20 }}>
+      <Text
+        fontWeight="bold"
+        size="h3"
+        style={{
+          alignSelf: "center",
+        }}
+      >
+        Height
+      </Text>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          paddingHorizontal: "30%",
+        }}
+      >
+        <TextInput
+          containerStyle={{ marginVertical: 20 }}
+          placeholder="00"
+          value={height}
+          autoCapitalize="none"
+          autoCompleteType="off"
+          autoCorrect={false}
+          keyboardType="number-pad"
+          onChangeText={(text: string) => setHeight(text)}
+          style={{ flex: 1 }}
+        />
+        <Text fontWeight="bold" size="lg" style={{ marginLeft: 10 }}>
+          CM
+        </Text>
+      </View>
+      <NextButton loading={loading} onPress={handleNext} />
     </View>
   );
 };
